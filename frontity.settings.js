4c4e7154f8c7f0db5e1c54c8fe5ffdd0abd2315d
nandapuspitarana <nandapuspitarana@gmail.com>
2021-04-24 13:04:55 +0700

    initial boilerplate testing

---
+++ frontity.settings.js
@@ -0,0 +1,71 @@
+const settings = {
+  "name": "wa-hello",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "my-first-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "https://test.frontity.org",
+          "postTypes": [
+            {
+              type: "destinations",
+              endpoint: "destinations",
+              archive: "/destinations"
+            }
+          ]
+          // "postTypes": [
+          //   {
+          //     type: "product",
+          //     endpoint: "product",
+          //     archive: "/product"
+          //   },
+          // ]
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
